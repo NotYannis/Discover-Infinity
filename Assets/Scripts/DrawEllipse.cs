9f281ecd7087ce3900e993cd2afd99e67705a7ec
ATTARD Yannis <Yannis.attard@formation.enjmin.fr>
2017-10-10 14:31:22 +0200

    Added planet ellipse and renderer update on  editor

---
+++ Assets/Scripts/DrawEllipse.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Planet))]
+public class DrawEllipse : Editor {
+
+    Planet planet;
+
+    SerializedProperty mass;
+    SerializedProperty minDistance;
+    SerializedProperty maxDistance;
+    SerializedProperty gravityForce;
+    SerializedProperty planetVelocity;
+    SerializedProperty systemCenter;
+    SerializedProperty semiminor;
+    SerializedProperty semimajor;
+
+    // Use this for initialization
+    void OnEnable () {
+        planet = (Planet)target;
+        mass = serializedObject.FindProperty("mass");
+        minDistance = serializedObject.FindProperty("minDistance");
+        maxDistance = serializedObject.FindProperty("maxDistance");
+        gravityForce = serializedObject.FindProperty("gravityForce");
+        planetVelocity = serializedObject.FindProperty("planetVelocity");
+        systemCenter = serializedObject.FindProperty("systemCenter");
+        semiminor = serializedObject.FindProperty("semiminor");
+        semimajor = serializedObject.FindProperty("semimajor");
+	}
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(mass);
+        EditorGUILayout.PropertyField(minDistance);
+        EditorGUILayout.PropertyField(maxDistance);
+        EditorGUILayout.PropertyField(gravityForce);
+        EditorGUILayout.PropertyField(planetVelocity);
+        EditorGUILayout.PropertyField(systemCenter);
+        EditorGUILayout.PropertyField(semiminor);
+        EditorGUILayout.PropertyField(semimajor);
+        serializedObject.ApplyModifiedProperties();
+        planet.UpdateEllipse();
+        planet.UpdatePlanetRenderer();
+    }
+}
--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -21,8 +21,8 @@ public class Planet : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     public Vector2 systemCenter = Vector2.zero;
     public float semimajor;
     public float semiminor;
-    public float ellipsePos = 0.0f;
-    public float ellipseVelocity = 0.1f;
+    public float planetVelocity = 0.1f;
+    private float ellipsePos = 0.0f;
 
     //Ellipse renderer
     private LineRenderer ellipseRenderer;
@@ -30,32 +30,40 @@ public class Planet : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     void Start()
     {
         mainCamera = Camera.main.GetComponent<CameraScript>();
+    }
 
-        maxDistance = mass * 5;
-        transform.localScale *= mass;
-
-        gravityField = GameObject.Find(gameObject.name + "/gravity");
-        gravityField.transform.localScale = Vector3.one;
-        gravityField.transform.localScale = new Vector3(maxDistance/gravityField.transform.lossyScale.x,
-                                                        maxDistance/ gravityField.transform.lossyScale.y, 1.0f);
-        minDistance = GetComponent<SpriteRenderer>().size.magnitude;
-
+    public void UpdateEllipse()
+    {
         //Ellipse points;
         float imax = 10;
-        Vector3[] points = new Vector3[(int)imax + 1 ];
-        for(float i = 1; i <= imax + 1; ++i)
+        Vector3[] points = new Vector3[(int)imax + 1];
+        for (float i = 1; i <= imax + 1; ++i)
         {
             float x = systemCenter.x + (semimajor * Mathf.Cos(Mathf.PI * (i / (imax / 2.0f))));
             float y = systemCenter.y + (semiminor * Mathf.Sin(Mathf.PI * (i / (imax / 2.0f))));
             points[(int)i - 1] = new Vector3(x, y, transform.position.z);
         }
 
-        
+
         Vector3[] smoothPoints = LineSmoother.SmoothLine(points, 0.1f);
 
         ellipseRenderer = GetComponent<LineRenderer>();
         ellipseRenderer.positionCount = smoothPoints.Length;
         ellipseRenderer.SetPositions(smoothPoints);
+
+        transform.position = points[0];
+    }
+
+    public void UpdatePlanetRenderer()
+    {
+        maxDistance = mass * 5;
+        transform.localScale = new Vector3(mass * 1.5f, mass * 1.5f, 0.0f);
+
+        gravityField = GameObject.Find(gameObject.name + "/gravity");
+        gravityField.transform.localScale = Vector3.one;
+        gravityField.transform.localScale = new Vector3(maxDistance / gravityField.transform.lossyScale.x,
+                                                        maxDistance / gravityField.transform.lossyScale.y, 1.0f);
+        minDistance = GetComponent<SpriteRenderer>().size.magnitude;
     }
 
     void Update()
@@ -69,7 +77,7 @@ public class Planet : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
 
         if (!isDragged)
         {
-            ellipsePos += ellipseVelocity;
+            ellipsePos += planetVelocity;
         }
     }
 
@@ -128,17 +136,17 @@ public class Planet : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
         Vector2 nextPos = (Vector2)EllipseNextPosition() - lastPos;
 
         float angle = Vector2.Angle(nextPos, deltaPos);
-        int dragSpeed = 60;
+        int dragSpeed = 5;
         float velocity = 1 - (angle / 90);
         float ellipsePosTemp = ellipsePos;
 
         if (goBack)
         {
-            ellipsePos -= ellipseVelocity * velocity * dragSpeed;
+            ellipsePos -= planetVelocity * velocity * dragSpeed;
         }
         else
         {
-            ellipsePos += ellipseVelocity * velocity * dragSpeed;
+            ellipsePos += planetVelocity * velocity * dragSpeed;
         }
 
         if (ellipsePos < ellipsePosTemp && !goBack)
